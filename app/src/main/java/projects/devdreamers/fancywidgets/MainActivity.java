d30bf6a54d488ea03632dafd22d33972f08db9dd
Jose Luis Crisostomo <jl.cs606@gmail.com>
2016-02-16 10:01:25 -0500

    Initial commit

---
+++ app/src/main/java/projects/devdreamers/fancywidgets/MainActivity.java
@@ -0,0 +1,14 @@
+package projects.devdreamers.fancywidgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //http://www.androidauthority.com/create-simple-android-widget-608975/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/projects/devdreamers/fancywidgets/Widget/WidgetHandler.java
@@ -0,0 +1,43 @@
+package projects.devdreamers.fancywidgets.Widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import projects.devdreamers.fancywidgets.R;
+
+/**
+ * Created by admin on 2/16/2016.
+ */
+public class WidgetHandler extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.content_widget);
+            remoteViews.setTextViewText(R.id.textView, number);
+
+            Intent intent = new Intent(context, WidgetHandler.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
